ba24edbaa47d2d9c912baf890488570a49d12738
AlexJFisher94 <alexjfisher1994@gmail.com>
2022-02-28 00:12:54 -0500

    Finished basic logic for game and updated UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,95 +1,109 @@
 'use strict';
 
-const gameChoices = ['rock', 'paper', 'scissors'];
+
+let rounds = document.querySelector('#round');
+let winsLosses = document.querySelector('#wins-losses');
 let playerScore = 0;
 let computerScore = 0;
-let result;
-/*
-button = document.querySelectorAll('.weapons');
-button.addEventListener('mouseover', focus);
+let computerSelection;
+let playerSelection;
+let round = 0;
+const weaponsButtons = document.querySelectorAll('.weapons');
+const hammer = document.querySelector('#hammer');
+const armor = document.querySelector('#armor');
+const spear = document.querySelector('#spear');
+const battleAlerts = document.querySelector('#battle-alert')
+const enemyChoice = document.querySelector('#enemy-choice');
+const playAgain = document.querySelector('.play-again');
 
-function focus(event) {
-    item.setAttribute("style", "background-color:blue;")
+// counts rounds
+function countRounds () {
+    round += 1;
+    rounds.textContent = `Round: ${round}`;
+    return round;
 }
-*/
+
 
 //randomly generates a choice for computer
 function computerPlay() {
+    const gameChoices = ['Hammer', 'Armor', 'Spear'];
     let computerSelection = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    enemyChoice.textContent = computerSelection
     return computerSelection;
+};
+
+// starts game and gets player's choice
+function playGame () {
+    let playerSelection;
+  weaponsButtons.forEach((weapon) => {
+    weapon.addEventListener('click', () => {
+      if (weapon.classList.contains('hammer')) {
+      playerSelection = 'Hammer';
+      } else if (weapon.classList.contains('armor')) {
+      playerSelection = 'Armor';
+      } else {
+      playerSelection = 'Spear';
+      }
+      computerSelection = computerPlay();
+      countRounds();
+      playRound(playerSelection, computerSelection);
+      displayWinner(playerScore, computerScore);
+      endGame(playerScore, computerScore);
+      resetGame();
+      console.log(playerSelection, computerSelection);
+    });
+  });
 }
 
-//gets player's choice
-function getPlayerChoice(userInput) {
-    userInput = prompt('Rock, paper, or scissors?');
-    let playerChoice = userInput.toLowerCase();
-    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
-        return playerChoice
-    } else {
-    return 'Oops! Try again.';
-  }
-}
 
-//plays a single round of rock, paper, scissors
-function playRound(playerSelection, computerSelection) {
-    playerSelection = getPlayerChoice();
-    computerSelection = computerPlay();
-    if (playerSelection === computerSelection) {
-        result = 'tie';
-        return 'It\'s a draw!';
-    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        result = 'lose';
-        return 'You lose! Paper beats rock.';
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        result = 'win';
-        return 'You win! Rock beats scissors.';
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        result = 'lose';        
-        return 'You lose! Scissors beats paper.';
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        result = 'win';
-        return 'You win! Paper beats rock.';
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        result = 'lose';
-        return 'You lose! Rock beats scissors.';
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        result = 'win';
-        return 'You win! Scissors beats paper.';
-  }
-}
-
-//plays a full game of rock, paper, scissors (5 rounds)
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
-    if (result === 'win') {
-        ++playerScore;
-    } else if (result === 'lose') {
-        ++computerScore;
+// plays one round
+function playRound (playerSelection, computerSelection) {
+    switch (true) {
+        case (playerSelection === computerSelection):
+            battleAlerts.textContent = 'It\'s a tie.';
+        break;
+        case (playerSelection === 'Hammer' && computerSelection === 'Armor'):
+        case (playerSelection === 'Armor' && computerSelection === 'Spear'):
+        case (playerSelection === 'Spear' && computerSelection === 'Hammer'):
+            playerScore++;
+            winsLosses.textContent = `Wins: ${playerScore} | Losses: ${computerScore}`;
+            battleAlerts.textContent = 'You win.';
+        break;
+        default:
+            computerScore++;
+            winsLosses.textContent = `Wins: ${playerScore} | Losses: ${computerScore}`;
+            battleAlerts.textContent = 'You lose.';
     }
 }
-     displayWinner();
-}
 
-//calculates and displays winner of game
-function displayWinner() {
-if (playerScore > computerScore) {
-    console.log('Player wins!');
-} else if (playerScore < computerScore) {
-    console.log('Computer wins!');
-} else if (playerScore === computerScore) {
-    console.log('It\'s a draw! Nobody wins.');
-  }
+function displayWinner (playerScore, computerScore) {
+    if (playerScore === 5) {
+    battleAlerts.textContent = 'Victory. It looks like you\'ll live to see another day.'
+    } else if (computerScore === 5) {
+        battleAlerts.textContent = 'Defeat. It looks like this battle was too much for you.'
+    }
 }
 
 
+function endGame (playerScore, computerScore) {
+    if (playerScore === 5 || computerScore === 5) {
+        weaponsButtons.forEach((button) => {
+            button.setAttribute('disabled', '');
+            button.classList.remove('focus');
+            playAgain.removeAttribute('hidden');
+          });
+    }
+}
+
+function resetGame () {
+    playAgain.addEventListener('click', () => {
+        window.location.reload();
+      });
+    }
 
 
 
-
-
-console.log(playerScore, computerScore);
-
+playGame();
 
 
 
